9ea6050b2fe93ad4fb049f184f1b7e58e9993e53
Koltunov Alexander <firstsano@gmail.com>
2017-05-26 12:41:00 +0300

    Add test for user to be an extension of activeresource

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'User', type: :model do
+  context 'using ActiveResource as user model' do
+    it 'should inherit from ActiveResource class' do
+      expect(User).to be <  ActiveResource::Base
+    end
+  end
 end
