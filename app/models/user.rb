403b131d766817240ad1d56f76e8132b6f1471e1
Koltunov Alexander <firstsano@gmail.com>
2017-05-27 16:20:41 +0300

    Add method to send real custom requests. Change authenticate method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,20 @@ class User < ActiveResource::Base
 
   def authenticate(password)
     self.password = password
-    response = parse_response self.class.post(:login)
+    response = parse_response send_custom_request(request_type: :post, element: 'customer-session', action: :login)
     return false unless response[:success]
     load response[:data]
     self
   end
 
   private
+
+  def send_custom_request(request_type:, action:, element: self.class.name.downcase)
+    self.class.element_name = element
+    response = self.class.send(request_type, action)
+    self.class.element_name = "user"
+    response
+  end
 
   def parse_response(response)
     JSON.parse(response.body).with_indifferent_access
