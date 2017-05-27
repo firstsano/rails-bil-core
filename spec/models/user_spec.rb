0a9de2916dd544afbf3a17205f76c2cfebf925df
Koltunov Alexander <firstsano@gmail.com>
2017-05-27 19:58:12 +0300

    Change examples for authenticate user to expect using service

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,26 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "User", type: :model do
   describe "using ActiveResource as user model" do
-    let(:json_headers) { {"Content-Type"=>"application/json"} }
-
-    def target_url(postfix)
-      ENV["volgaspot_api_prefix"] + postfix
-    end
-
     it "should inherit from ActiveResource class" do
       expect(User).to be <  ActiveResource::Base
     end
 
     describe "instance methods" do
       context "#authenticate" do
-        let(:user_attributes) { {id: 1, name: "John Doe", email: "john@mail.com"} }
+        let(:user_attributes) { {name: "John Doe", login: "riky"} }
+        let(:db_user_attributes) { user_attributes.merge({id: 1}) }
         let(:user_password) { "test_password" }
-        let(:current_user) { User.new }
+        let(:current_user) { User.new(user_attributes) }
 
-        def mock_response(output = {})
-          ActiveResource::HttpMock.respond_to do |mock|
-            mock.post target_url("/customer-sessions/login"), json_headers, output.to_json
-          end
+        def mock_service(output: {})
+          allow(VolgaspotApiService).to receive(:login).and_return(output)
         end
 
         it "should respond to a method with password param" do
@@ -29,14 +22,14 @@ RSpec.describe "User", type: :model do
         end
 
         it "should return user if remote server has one with valid credentials" do
-          mock_response({success: true, data: user_attributes})
+          mock_service output: db_user_attributes
           authenticated_user = current_user.authenticate(user_password)
           expect(authenticated_user).to be_truthy
           expect(authenticated_user.id).to eq(1)
         end
 
         it "should return false if no user found on remote server with given credentials" do
-          mock_response({})
+          mock_service output: false
           expect(current_user.authenticate(user_password)).to be false
         end
       end
