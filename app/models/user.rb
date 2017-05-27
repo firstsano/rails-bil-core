7333bb3f1747646702fc196f424604536b3979d7
Koltunov Alexander <firstsano@gmail.com>
2017-05-27 13:26:29 +0300

    Implement authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveResource::Base
-  self.site = ENV["volgaspot_api"] + "/users"
+  self.site = ENV["volgaspot_api"]
+
+  def authenticate(password)
+    self.password = password
+    response = parse_response self.class.post(:login)
+    return false unless response[:success]
+    load response[:data]
+    self
+  end
+
+  private
+
+  def parse_response(response)
+    JSON.parse(response.body).with_indifferent_access
+  end
 end
