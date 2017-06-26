53a040c4312a66bd14dea2ed8a73f3ac8ccf91c3
Koltunov Alexander <firstsano@gmail.com>
2017-06-26 18:03:55 +0300

    Change class to module, and class_attr to constant

--- app/services/volgaspot_api_service.rb
+++ app/services/volgaspot_api_service.rb
@@ -1,12 +1,13 @@
-class VolgaspotApiService
+module VolgaspotApiService
   include HTTParty
   base_uri ENV["volgaspot_api"]
 
-  class_attribute :routes
-  self.routes = { login: "/customer-sessions/login" }
+  ROUTES = {
+    login: "/customer-sessions/login"
+  }
 
   def self.login(login:, password:)
-    raw_response = put routes[:login], body: { login: login, password: password }
+    raw_response = put ROUTES[:login], body: { login: login, password: password }
     response = raw_response.parsed_response.with_indifferent_access
     return false unless response[:success]
     response[:data]
