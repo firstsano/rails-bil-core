d982458cefcb5f39e6b24f59b4ec2f3bcefce5e9
Koltunov Alexander <firstsano@gmail.com>
2017-06-30 18:42:17 +0300

    Refactor to use lets

--- spec/routing/api/v1/account_spec.rb
+++ spec/routing/api/v1/account_spec.rb
@@ -1,14 +1,16 @@
 RSpec.describe "routes user data", type: :routing do
-  let(:current_namespace) { "api/v1" }
+  let(:namespace) { "api/v1" }
 
   describe "user detalization" do
+    let(:path) { namespace + "/account/discounts" }
+
     it "should be routable" do
-      expect(get: namespaced("/account/discounts")).to be_routable
+      expect(get: path).to be_routable
     end
 
     it "should route to specific controller" do
-      expect(get: namespaced("/account/discounts")).to route_to(
-        controller: namespaced("/account"),
+      expect(get: path).to route_to(
+        controller: namespace + "/account",
         action: "discounts"
       )
     end
