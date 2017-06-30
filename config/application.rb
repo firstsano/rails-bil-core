776fd765d2c5ed6998bcf778b7d8391a65df84ce
Koltunov Alexander <firstsano@gmail.com>
2017-06-30 15:00:30 +0300

    Do not generate spec for controllers, test requests

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,10 @@ module RailsCoreApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Set up spec generators
+    config.generators do |g|
+      g.controller_specs false
+    end
   end
 end
