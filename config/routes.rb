cb6162b885ec42f85d1b237ce00852689d970f2d
Koltunov Alexander <firstsano@gmail.com>
2017-06-30 14:52:15 +0300

    Move controller and change routes to use namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :users do
-    collection do
-      post 'sign-in' => 'user_token#create'
+  namespace :api do
+    namespace :v1 do
+      post '/users/sign-in' => 'user_token#create'
+      get '/account/discounts'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
