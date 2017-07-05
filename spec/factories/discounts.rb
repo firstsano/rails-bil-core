7a472fe72132a897ad44bef0b9e4d1d1b7e486cc
Koltunov Alexander <firstsano@gmail.com>
2017-07-05 19:04:18 +0300

    Add factory for discount model

---
+++ spec/factories/discounts.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :discount do
+    account_id { Faker::Number.number(6) }
+    incoming_rest { Faker::Number.decimal(2) }
+    outgoing_rest { Faker::Number.decimal(2) }
+    discount { Faker::Number.decimal(2) }
+    discount_with_tax { discount * 1.2 }
+    service_id { Faker::Number.between(1, 20) }
+    service_type { Faker::Number.between(1, 20) }
+    discount_period_id { Faker::Number.between(1, 20) }
+    slink_id { Faker::Number.between(1, 20) }
+    discount_date { Faker::Time.to_i }
+    charge_type { Faker::Number.between(1, 20) }
+  end
+end
