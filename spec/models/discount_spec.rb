4ecc2abf9e7e393f976090027d1508f37a9c445a
Koltunov Alexander <firstsano@gmail.com>
2017-07-05 19:04:37 +0300

    Add discount model test file

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Discount, type: :model do
+  describe "read-only records class" do
+    it "should not be able to save records" do
+      
+    end
+    it "should not be able to update records" do
+
+    end
+    it "should not be able to destroy records" do
+
+    end
+  end
+end
