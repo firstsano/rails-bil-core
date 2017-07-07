9a05dcbe93a65b82944e37cf22c1e2cb67e873e5
Koltunov Alexander <firstsano@gmail.com>
2017-07-07 19:45:53 +0300

    Add scope

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,8 @@
 class Discount < VolgaspotRecord
   self.table_name = "discount_transactions_all"
 
+  scope :by_user_account, -> user { where(account_id: user.account_id) }
+
   def readonly?
     true
   end
