dd080918094becec9b541da1cfdf5a3f4dd412fd
Koltunov Alexander <firstsano@gmail.com>
2017-07-07 19:45:32 +0300

    Add user schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ActiveResource::Base
   class_attribute :auth_service
   self.auth_service = VolgaspotApiService
 
+  schema do
+    string 'login', 'full_name', 'actual_address', 'status', 'mobile_phone', 'email'
+    integer 'account_id'
+  end
+
   def self.from_token_request(request)
     login = request.params["auth"] && request.params["auth"]["login"]
     self.new login: login
