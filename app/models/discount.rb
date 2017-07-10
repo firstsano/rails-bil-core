5c343274d3c8152a298282cc2a3de0daac5331af
Koltunov Alexander <firstsano@gmail.com>
2017-07-10 17:35:58 +0300

    Add Service model and spec

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,8 +2,4 @@ class Discount < VolgaspotRecord
   self.table_name = "discount_transactions_all"
 
   scope :by_user_account, -> user { where(account_id: user.account_id) }
-
-  def readonly?
-    true
-  end
 end
---
+++ spec/models/service_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Service, type: :model do
+  it { is_expected.to be_readonly }
+end
