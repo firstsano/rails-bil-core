2cdfc5b8117c935b783d54a4842c415693721083
Koltunov Alexander <firstsano@gmail.com>
2017-07-12 13:40:49 +0300

    Refactor shared examples

--- spec/requests/v1/account_spec.rb
+++ spec/requests/v1/account_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe V1::AccountController, type: :request do
   describe "GET discounts" do
     let(:route) { "/account/discounts" }
+    let(:json_auth_headers) { json_headers.merge auth_headers }
     before { get route }
 
     context "when user is not authorized" do
@@ -16,16 +17,15 @@ RSpec.describe V1::AccountController, type: :request do
       before do
         5.times { create :discount, account_id: @user.account_id }
         5.times { create :discount }
-        get route, headers: json_headers.merge(auth_headers)
+        get route, headers: json_auth_headers
       end
 
-      it "should return corresponding data in response", aggregate_failures: true do
-        expect(response).to have_http_status(:ok)
+      it_behaves_like 'basic json API response', should_have_items: 5,
+        required_attributes: ["incoming-rest", "outgoing-rest", "discount", "discount-with-tax", "discount-date"]
+      it_behaves_like 'json API response with relationships', "service"
+      it_behaves_like 'json API response with included' do
+        before { get route, params: { include: :service }, headers: json_auth_headers }
       end
-
-      it_behaves_like 'json API response', should_have_items: 5,
-        required_attributes: ["incoming-rest", "outgoing-rest", "discount", "discount-with-tax", "discount-date"],
-        with_relationships: "service"
     end
   end
 end
