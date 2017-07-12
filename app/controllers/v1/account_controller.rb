47afbf78745dce41cabed26a01f00d0247072d65
Koltunov Alexander <firstsano@gmail.com>
2017-07-12 13:41:10 +0300

    Add render wrapper

--- app/controllers/v1/account_controller.rb
+++ app/controllers/v1/account_controller.rb
@@ -1,6 +1,6 @@
 class V1::AccountController < V1::BaseController
   def discounts
     @discounts = Discount.by_user_account current_user
-    render json: @discounts
+    render @discounts
   end
 end
--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -2,4 +2,13 @@ class V1::BaseController < ActionController::API
   include Knock::Authenticable
 
   before_action :authenticate_user
+
+  def render(resource)
+    super json: resource, include: inclusion_filter
+  end
+
+  def inclusion_filter
+    return [] unless params[:include]
+    params[:include].split(/[,\.]/).uniq
+  end
 end
