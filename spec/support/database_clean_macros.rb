9cb77a3d67dd41e8da2a878364a27ec1d4ba619d
Koltunov Alexander <firstsano@gmail.com>
2017-07-13 16:25:32 +0300

    Change database cleaning strategy and add helpers, due to database_cleaner multi db transaction issue

---
+++ spec/support/database_clean_macros.rb
@@ -0,0 +1,12 @@
+module DatabaseCleanHelpers
+  def clean
+    DatabaseCleaner.clean
+  end
+
+  def clean_before(type = :all, &block)
+    before(type) do
+      DatabaseCleaner.clean
+      self.instance_eval(&block) if block
+    end
+  end
+end
