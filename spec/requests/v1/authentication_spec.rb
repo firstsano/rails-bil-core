83767aaad8df07808eaf8b67ff6cb477b8269fea
Koltunov Alexander <firstsano@gmail.com>
2017-07-16 15:41:02 +0300

    Set up athentication testing

---
+++ spec/requests/v1/authentication_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe V1::BaseController, type: :request do
+  describe "authorization" do
+    setup_auth
+    let(:route) { "/account/discounts" }
+
+    context "when user is not authorized" do
+      it "should return not_authorized status" do
+        get route, headers: json_headers
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context "when user is authorized through token" do
+      it "should return resource with ok status" do
+        get route, headers: auth_headers
+        expect(response).to have_http_status(:ok)
+        expect(json_body).not_to be_empty
+      end
+    end
+  end
+end
