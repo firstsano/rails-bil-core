7c06d6badfb6ff61ecdbc17fabb5dd00e9acca2e
Koltunov Alexander <firstsano@gmail.com>
2017-07-16 15:41:02 +0300

    Set up athentication testing

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,5 +1,5 @@
 module Authentication
-  def sign_in
+  def setup_auth
     before(:all) do
       @user ||= build :user
       @user_token = Knock::AuthToken.new(payload: { sub: @user.id }).token
@@ -7,10 +7,16 @@ module Authentication
 
     before do
       target = ENV["volgaspot_api"] + "/users/#{@user.id}"
-      user_response = { user: @user.to_h }
-      stub_request(:get, target).to_return body: user_response.to_json
+      user_response = { success: true, data: @user.to_h }.to_json
+      stub_request(:get, target).to_return body: user_response,
+        headers: { "Content-Type" => "application/json" }
     end
 
-    let(:auth_headers) { { "Authorization": "Bearer #{@user_token}" } }
+    let(:auth_headers) {
+      {
+        "HTTP_ACCEPT" => "application/json",
+        "Authorization": "Bearer #{@user_token}"
+      }
+    }
   end
 end
