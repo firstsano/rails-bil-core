8405c61f72f50442e75f95d5774f2a66d00c2d1b
Koltunov Alexander <firstsano@gmail.com>
2017-07-18 12:43:56 +0300

    Refactor login macros

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,16 +1,15 @@
 module Authentication
   def setup_auth
     before(:all) do
-      @user_fetch_response ||= build :vs_response, :user
-      @user ||= User.new @user_fetch_response[:data]
-      @user.utm_account = @user_fetch_response[:data][:account][:id]
+      @user_response ||= build :vs_response, :user
+      @user ||= User.new.set_attributes @user_response[:data]
+      @user.utm_account = @user_response[:data][:account][:id]
       @user_token = Knock::AuthToken.new(payload: { sub: @user.id }).token
     end
 
     before do
       target = ENV["volgaspot_api"] + "/users/#{@user.id}?expand=account"
-      user_response = @user_fetch_response.to_json
-      stub_request(:get, target).to_return body: user_response,
+      stub_request(:get, target).to_return body: @user_response.to_json,
         headers: { "Content-Type" => "application/json" }
     end
 
