b24e9fff983e0a552067d2b1996136397e15fbb2
Koltunov Alexander <firstsano@gmail.com>
2017-07-19 15:58:15 +0300

    Remove secrets.yml from git tracked

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,5 +20,5 @@ set :ssh_options, {
 
 set :passenger_restart_with_touch, true
 
-append :linked_files, "config/application.yml"
+append :linked_files, "config/application.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp"
