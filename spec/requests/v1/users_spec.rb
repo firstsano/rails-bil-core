ea28f51551cf2bf85fe1c4359ef1c39f34243c01
Koltunov Alexander <firstsano@gmail.com>
2017-07-24 15:18:26 +0300

    Set up user show action with tests

--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -4,31 +4,31 @@ RSpec.describe V1::UsersController, type: :request do
   before { User.remote_data_service = VolgaspotApiService }
   setup_auth
 
-  # describe "GET status" do
-  #   let(:route) { "/users/show" }
-  #   before { get route, headers: auth_headers }
-  #
-  #   it "should return success with data attribute", aggregate_failures: true do
-  #     expect(response).to have_http_status(:ok)
-  #     expect(response.body).to match_json_expression({ data: Object })
-  #   end
-  #
-  #   it "should return specific account attributes" do
-  #     pattern = {
-  #       data: {
-  #         attributes: {
-  #           login: String,
-  #           email: String,
-  #           balance: wildcard_matcher,
-  #           "account-id" => wildcard_matcher,
-  #           "full-name" => String,
-  #           "actual-address" => String,
-  #           "mobile-phone" => String,
-  #           "internet-status" => wildcard_matcher
-  #         }
-  #       }
-  #     }
-  #     expect(response.body).to match_json_expression pattern
-  #   end
-  # end
+  describe "GET show" do
+    let(:route) { "/users/show" }
+    before { get route, headers: auth_headers }
+
+    it "should return success with data attribute", aggregate_failures: true do
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to match_json_expression({ data: Object })
+    end
+
+    it "should return specific user attributes" do
+      pattern = {
+        data: {
+          attributes: {
+            login: String,
+            email: String,
+            balance: wildcard_matcher,
+            "full-name" => String,
+            "actual-address" => String,
+            "mobile-phone" => String,
+            "internet-status" => wildcard_matcher,
+            "account-id" => wildcard_matcher
+          }
+        }
+      }
+      expect(response.body).to match_json_expression pattern
+    end
+  end
 end
