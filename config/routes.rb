0f2a200a63ff0092e1dc40a598a945bd2d91feac
Koltunov Alexander <firstsano@gmail.com>
2017-07-24 18:25:58 +0300

    Add 'payments' controller and index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope module: :v1 do
     post '/users/sign-in' => 'user_token#create'
     get '/discounts/index'
+    get '/payments/index'
     get '/users/show'
   end
 end
