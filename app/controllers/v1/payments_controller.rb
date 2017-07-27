65b179c3af091fc59ce153b29a35a0ebf012a556
Koltunov Alexander <firstsano@gmail.com>
2017-07-27 18:26:03 +0300

    Add filter processing

--- app/controllers/v1/payments_controller.rb
+++ app/controllers/v1/payments_controller.rb
@@ -1,5 +1,19 @@
 class V1::PaymentsController < V1::BaseController
+  before_action :get_date_interval
+
   def index
-    render Payment.by_user_account(current_user.utm_account)
+    @payments = Payment
+      .includes(:payment_method)
+      .date_interval(from: @from, to: @to)
+      .by_user_account current_user.utm_account
+    render @payments
+  end
+
+  private
+
+  def get_date_interval
+    @from, @to = filter_params :from, :to
+    @from = @from.try(:to_datetime) || DateTime.now.beginning_of_month
+    @to = @to.try(:to_datetime) || DateTime.now.beginning_of_day
   end
 end
