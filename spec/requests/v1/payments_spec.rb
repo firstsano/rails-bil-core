b3bd0fc1cf10f9028ca773586ab2f9cf3f5239f0
Koltunov Alexander <firstsano@gmail.com>
2017-07-27 18:06:11 +0300

    Set up base api request covering

--- spec/requests/v1/payments_spec.rb
+++ spec/requests/v1/payments_spec.rb
@@ -9,7 +9,21 @@ RSpec.describe V1::PaymentsController, type: :request do
     let(:route) { "/payments/index" }
 
     context "payments requested with filter" do
-      it_behaves_like 'basic json API response'
+      clean_before(:all) do
+        # need to do some magic due to safe attributes
+        # and 'save' InvalidArgument error
+        create_list :payment, 20
+        create_list :payment, 20, account_id: @user.utm_account
+        payment_method = create :payment_method
+        Payment.update_all method: payment_method.id
+      end
+      before { get route, params: { include: "payment-method" }, headers: auth_headers }
+
+      it_behaves_like 'basic json API response',
+        should_have_items: 20,
+        required_attributes: ["payment-incurrency"]
+      it_behaves_like 'json API response with included', "payment-methods"
+      it_behaves_like 'json API response with relationships', "payment-method"
     end
 
     after(:all) { Timecop.return }
