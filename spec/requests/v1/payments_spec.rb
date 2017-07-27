c4a0e045d5cef5a635378fb8ed093085ff315901
Koltunov Alexander <firstsano@gmail.com>
2017-07-27 18:24:43 +0300

    Set up payments request test

--- spec/requests/v1/payments_spec.rb
+++ spec/requests/v1/payments_spec.rb
@@ -9,18 +9,25 @@ RSpec.describe V1::PaymentsController, type: :request do
     let(:route) { "/payments/index" }
 
     context "payments requested with filter" do
+      def discount_range
+        ((Date.today - 1.month)..Date.today).to_a
+      end
+
+      let(:from) { discount_range[5] }
+      let(:to) { discount_range[10] }
+
       clean_before(:all) do
         # need to do some magic due to safe attributes
         # and 'save' InvalidArgument error
         create_list :payment, 20
-        create_list :payment, 20, account_id: @user.utm_account
+        discount_range.map(&:to_time).map(&:to_i).each { |d| create :payment, account_id: @user.utm_account, payment_enter_date: d }
         payment_method = create :payment_method
         Payment.update_all method: payment_method.id
       end
-      before { get route, params: { include: "payment-method" }, headers: auth_headers }
+      before { get route, params: { include: "payment-method", filter: { from: from, to: to } }, headers: auth_headers }
 
       it_behaves_like 'basic json API response',
-        should_have_items: 20,
+        should_have_items: 5,
         required_attributes: ["payment-incurrency"]
       it_behaves_like 'json API response with included', "payment-methods"
       it_behaves_like 'json API response with relationships', "payment-method"
