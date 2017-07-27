253748e6264ed10b00ad1918d00ff8391a6137a5
Koltunov Alexander <firstsano@gmail.com>
2017-07-27 18:25:09 +0300

    Add method to select payments by date

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -6,4 +6,10 @@ class Payment < VolgaspotRecord
   has_one :payment_method, primary_key: "method", foreign_key: "id"
 
   scope :by_user_account, -> account { where(account_id: account.id) }
+
+  def self.date_interval(from: nil, to: nil)
+    scoped = self
+    scoped = from ? scoped.where.has { payment_enter_date >= from.to_i } : scoped
+    scoped = to ? scoped.where.has { payment_enter_date <= to.to_i } : scoped
+  end
 end
