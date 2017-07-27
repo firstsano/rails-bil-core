ea5cd4e8c547028710578cb27c0a1176a8f2b41f
Koltunov Alexander <firstsano@gmail.com>
2017-07-27 18:06:26 +0300

    Add scope

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,6 @@ class Payment < VolgaspotRecord
   self.table_name = "payment_transactions"
 
   has_one :payment_method, primary_key: "method", foreign_key: "id"
+
+  scope :by_user_account, -> account { where(account_id: account.id) }
 end
