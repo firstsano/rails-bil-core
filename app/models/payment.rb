3f28aabb1aa6b294403601875b0754a3f9d85001
Koltunov Alexander <firstsano@gmail.com>
2017-07-27 14:34:27 +0300

    Add relation to Payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,6 @@ class Payment < VolgaspotRecord
   include SafeAttributes::Base
 
   self.table_name = "payment_transactions"
+
+  has_one :payment_method, primary_key: "method", foreign_key: "id"
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
   it { is_expected.to be_readonly }
+
+  describe "instance methods" do
+    context "#payment_method" do
+      subject { build :payment, :with_payment_method }
+      it { is_expected.to respond_to(:payment_method) }
+      its(:payment_method) { is_expected.to be_an_instance_of(PaymentMethod) }
+    end
+  end
 end
