92b3a4bc3e760d7fe50edd68ea109254e60065f3
Koltunov Alexander <firstsano@gmail.com>
2017-07-27 14:34:04 +0300

    Reduce factory fields to only necessary

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -5,12 +5,6 @@ FactoryGirl.define do
     outgoing_rest { Faker::Number.decimal(2) }
     discount { Faker::Number.decimal(2) }
     discount_with_tax { discount * 1.2 }
-    service_id { Faker::Number.between(1, 20) }
-    service_type { Faker::Number.between(1, 20) }
-    discount_period_id { Faker::Number.between(1, 20) }
-    slink_id { Faker::Number.between(1, 20) }
-    discount_date { DateTime.now.to_i }
-    charge_type { Faker::Number.between(1, 20) }
     service
 
     after(:build) do |discount|
