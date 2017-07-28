5e59984569b5738ad64e7d2bf008eedead6693d7
Koltunov Alexander <firstsano@gmail.com>
2017-07-28 17:48:06 +0300

    Add service_type_name method

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,8 @@ class Service < VolgaspotRecord
 
   alias_attribute :name, :service_name
   belongs_to :parent, foreign_key: :parent_service_id, class_name: 'Service', required: false
+
+  def service_type_name
+    parent.service_name if parent
+  end
 end
