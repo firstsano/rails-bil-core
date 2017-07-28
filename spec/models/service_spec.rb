43af251a26a857ea71335530340387149e06dd3c
Koltunov Alexander <firstsano@gmail.com>
2017-07-28 17:48:06 +0300

    Add service_type_name method

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  clean_before(:all)
+  clean_before
   it { is_expected.to be_readonly }
   it { is_expected.to respond_to(:name) }
 
@@ -10,6 +10,19 @@ RSpec.describe Service, type: :model do
       subject { create :service, :with_parent }
       it { is_expected.to respond_to(:parent) }
       its(:parent) { is_expected.to be_an_instance_of(Service) }
+    end
+
+    context "::service_type_name" do
+      it { is_expected.to respond_to(:service_type_name) }
+      context "when parent exists" do
+        subject { create :service, :with_parent }
+        its(:service_type_name) { is_expected.to eq subject.parent.name }
+      end
+
+      context "when parent does not exist" do
+        subject { create :service }
+        its(:service_type_name) { is_expected.to be_nil }
+      end
     end
   end
 end
