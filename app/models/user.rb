14eeadb1af8165810e374ed7b78958ed298c95d3
Koltunov Alexander <firstsano@gmail.com>
2017-07-28 15:08:19 +0300

    Move Account to User subclass

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,20 @@ class User
     [:id, :login, :full_name, :actual_address, :status,
       :mobile_phone, :email, :vist_account]
   end
+
+  class Account
+    include ActiveModel::Model
+    include ActiveModel::Serialization
+    include WhitelistAttributes
+
+    attr_accessor :id, :balance, :credit, :int_status, :is_blocked
+
+    alias_attribute :internet_status, :int_status
+
+    private
+
+    def whitelist_params
+      [:id, :balance, :credit, :int_status, :is_blocked]
+    end
+  end
 end
