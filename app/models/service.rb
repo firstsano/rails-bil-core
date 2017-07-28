3be9584978e8dff8a50dda82c77e0b7d4cf66c21
Koltunov Alexander <firstsano@gmail.com>
2017-07-28 17:25:42 +0300

    Add alias and relation

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,6 @@
 class Service < VolgaspotRecord
   self.table_name = "services_data"
+
+  alias_attribute :name, :service_name
+  belongs_to :parent, foreign_key: :parent_service_id, class_name: 'Service', required: false
 end
