ce6bc4ba383906cbd7b9f9bd2eba876c4153cf4e
Koltunov Alexander <firstsano@gmail.com>
2017-07-28 20:36:00 +0300

    Add attributes to serializer, add cost to Service

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,6 +4,12 @@ class Service < VolgaspotRecord
   alias_attribute :name, :service_name
   belongs_to :parent, foreign_key: :parent_service_id, class_name: 'Service', required: false
 
+  attr_accessor :cost_month
+
+  def cost_day
+    cost_month ? cost_month / 30 : 0
+  end
+
   def service_type_name
     parent.service_name if parent
   end
--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,3 +1,11 @@
 class ServiceSerializer < ActiveModel::Serializer
-  attributes :id, :service_name, :comment
+  attributes :id, :name, :service_type, :description, :cost_month, :cost_day
+
+  def service_type
+    object.service_type_name
+  end
+
+  def description
+    object.comment
+  end
 end
