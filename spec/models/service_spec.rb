cc9c05b49fa4bad1282392bbf5ddacdcf1f5928c
Koltunov Alexander <firstsano@gmail.com>
2017-07-28 17:25:25 +0300

    Test service's relation

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
+  clean_before(:all)
   it { is_expected.to be_readonly }
+  it { is_expected.to respond_to(:name) }
+
+  describe "instance methods" do
+    context "::parent" do
+      subject { create :service, :with_parent }
+      it { is_expected.to respond_to(:parent) }
+      its(:parent) { is_expected.to be_an_instance_of(Service) }
+    end
+  end
 end
