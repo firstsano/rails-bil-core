04bd54d518f26d30e5e396a0c1dfc73559db873e
Koltunov Alexander <firstsano@gmail.com>
2017-07-30 19:11:17 +0300

    Fix naming conventions

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Service, type: :model do
   clean_before(:all) { Timecop.freeze }
 
-  describe "service methods" do
-    context "::service_data" do
+  describe "instance methods" do
+    context "#service_data" do
       subject { build :service, :with_service_data }
       it { is_expected.to respond_to(:service_data) }
       its(:service_data) { is_expected.to be_an_instance_of(ServiceData) }
     end
 
-    context "::cost_day" do
+    context "#cost_day" do
       subject { build :service }
       let(:service) { subject }
 
@@ -31,7 +31,7 @@ RSpec.describe Service, type: :model do
   end
 
   describe "class methods" do
-    context "#new" do
+    context "::new" do
       it "should not create service without id specified" do
         expect{ Service.new }.to raise_error(ArgumentError)
       end
