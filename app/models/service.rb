48980099d49fc4ecb08022d7e5c57fcd66b82c58
Koltunov Alexander <firstsano@gmail.com>
2017-07-30 18:47:33 +0300

    Refactor specs, add serialization and base functionality to Service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,11 +1,23 @@
 class Service
-  attr_reader :id
+  include ActiveModel::Model
+  include ActiveModel::Serialization
+  include WhitelistAttributes
 
-  def initialize(id)
+  attr_reader :id
+  attr_accessor :cost_month
+
+  def initialize(id, params = {})
     @id = id
+    super params
   end
 
   def service_data
     @service_data ||= ServiceData.find id
+  end
+
+  private
+
+  def whitelist_params
+    [:cost_month]
   end
 end
