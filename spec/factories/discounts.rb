1d5a267314c9baaf461bb5a95a3bf05a6e41f314
Koltunov Alexander <firstsano@gmail.com>
2017-07-31 16:34:59 +0300

    Remove unnecessary fake data

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -1,11 +1,9 @@
 FactoryGirl.define do
   factory :discount, class: Discount do
-    account_id { Faker::Number.number(6) }
     incoming_rest { Faker::Number.decimal(2) }
     outgoing_rest { Faker::Number.decimal(2) }
     discount { Faker::Number.decimal(2) }
     discount_with_tax { discount * 1.2 }
-    service
 
     after(:build) do |discount|
       class << discount
