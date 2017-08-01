9dc2ffadf70f2b510fc11d153ec85bbf58b3c46a
Koltunov Alexander <firstsano@gmail.com>
2017-08-01 21:01:01 +0300

    Add cost loading from db

--- app/models/utm/periodic_service_data.rb
+++ app/models/utm/periodic_service_data.rb
@@ -1,3 +1,5 @@
 class Utm::PeriodicServiceData < Utm::VolgaspotRecord
   self.table_name = "periodic_services_data"
+
+  alias_attribute :cost_month, :cost
 end
--- spec/models/utm/periodic_service_data_spec.rb
+++ spec/models/utm/periodic_service_data_spec.rb
@@ -2,4 +2,11 @@ require "rails_helper"
 
 RSpec.describe Utm::PeriodicServiceData, type: :model do
   it_behaves_like "readonly model"
+
+  describe "instance methods" do
+    context "#cost_month" do
+      it { is_expected.to respond_to(:cost_month) }
+      its(:cost) { is_expected.to eq subject.cost_month }
+    end
+  end
 end
