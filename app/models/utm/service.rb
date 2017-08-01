31a2eafa41a4d98ca141df2a020e978d0ea15bbb
Koltunov Alexander <firstsano@gmail.com>
2017-08-01 21:01:01 +0300

    Add cost loading from db

--- app/models/utm/service.rb
+++ app/models/utm/service.rb
@@ -4,13 +4,20 @@ class Utm::Service < Utm::VolgaspotRecord
   alias_attribute :name, :service_name
   belongs_to :parent, foreign_key: :parent_service_id, class_name: 'Service', required: false
 
-  attr_writer :cost_month
+  PERIODIC_SERVICE_TYPES = [2, 5]
 
-  def cost_month
-    @cost_month || 0
+  delegate :cost_month, to: :service_data
+
+  def service_data
+    @service_data ||= Utm::PeriodicServiceData.find(id) if is_periodic_service?
+  end
+
+  def is_periodic_service?
+    PERIODIC_SERVICE_TYPES.include? service_type
   end
 
   def cost_day
+    raise Exceptions::EmptyRelationError.new unless service_data
     days_in_month = Time.days_in_month(Date.current.month)
     (cost_month.to_f / days_in_month).round 2
   end
--- spec/models/utm/service_spec.rb
+++ spec/models/utm/service_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Utm::Service, type: :model do
   it_behaves_like "readonly model"
 
   clean_before(:all) { Timecop.freeze }
+  let(:periodic_service_types) { [1, 2] }
+  let(:periodic_service_type) { periodic_service_types.sample }
+  before { stub_const("Utm::Service::PERIODIC_SERVICE_TYPES", periodic_service_types) }
 
   describe "instance methods" do
     subject { build :service }
@@ -23,6 +26,36 @@ RSpec.describe Utm::Service, type: :model do
       end
     end
 
+    context "#is_periodic_service?" do
+      it { is_expected.to respond_to(:is_periodic_service?) }
+
+      context "when service is periodic" do
+        subject { create :service, service_type: periodic_service_type }
+        it { is_expected.to be_is_periodic_service }
+      end
+
+      context "when service is not periodic" do
+        subject { create :service, service_type: 124141 }
+        it { is_expected.not_to be_is_periodic_service }
+      end
+    end
+
+    context "#service_data" do
+      it { is_expected.to respond_to(:service_data) }
+
+      it "should return PeriodicServiceData instance" do
+        service = create :service, service_type: 1
+        periodic_service_data = create :periodic_service_data, id: service.id
+        expect(service.service_data).to eq periodic_service_data
+      end
+
+      it "should return nil", aggregate_failures: true do
+        service = create :service, service_type: 6
+        periodic_service_data = create :periodic_service_data, id: service.id
+        expect(service.service_data).to be_nil
+      end
+    end
+
     context "#parent" do
       subject { create :service, :with_parent }
       it { is_expected.to respond_to(:parent) }
@@ -36,18 +69,26 @@ RSpec.describe Utm::Service, type: :model do
     context "#cost_day" do
       it { is_expected.to respond_to(:cost_day) }
 
-      context "when cost_month is set" do
+      context "when service_data exists" do
         it "should calculate cost_day based on cost_month" do
-          service.cost_month = 1000.0
+          service = create :service, service_type: periodic_service_type
+          periodic_service_data = create :periodic_service_data, id: service.id, cost: 1000
           expectable_cost = (1000.0 / Time.days_in_month(Date.current.month)).round 2
           expect(service.cost_day).to eq expectable_cost
         end
       end
 
       context "when cost_month is not set" do
-        its(:cost_day) { is_expected.to eq 0 }
+        it "should raise error" do
+          expect{ subject.cost_day }.to raise_error(Exceptions::EmptyRelationError)
+        end
       end
     end
+  end
+
+  describe "class methods" do
+    subject { described_class }
+    it { should be_const_defined(:PERIODIC_SERVICE_TYPES) }
   end
 
   after(:all) { Timecop.return }
