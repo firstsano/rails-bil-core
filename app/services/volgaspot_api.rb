c9a8b2232528df28287f6ce2ba8a000552d48c8e
Koltunov Alexander <firstsano@gmail.com>
2017-08-02 15:14:14 +0300

    Add route method and transformer submodule

--- app/services/volgaspot_api.rb
+++ app/services/volgaspot_api.rb
@@ -4,40 +4,48 @@ module VolgaspotApi
 
   ROUTES = {
     login: "/customer-sessions/login",
-    user_data: "/users/"
+    user_data: "/users/user_id?expand=account",
+    user_services: "/users/user_id?expand=services"
   }
 
   def self.login(login:, password:)
-    send_request put(ROUTES[:login], body: { login: login, password: password })
+    get_response put(route(:login), body: { login: login, password: password })
   end
 
   def self.fetch_user_data(id, user)
-    data = send_request get(ROUTES[:user_data] + id.to_s, query: { expand: :account })
-    data[:vist_account] = data[:account_id]
+    data = get_response get(route(:user_data, user_id: id))
+    ApiDataTranformer.transform_user_data data
     user.set_attributes data
     user.utm_account.set_attributes data[:account]
     user
   end
 
   def self.fetch_user_services(id)
-    data = send_request get(ROUTES[:user_data] + id.to_s, query: { expand: :services })
-    services = data[:services].values.index_by { |service| service[:service_id] }
-  end
-
-  def self.load_user_services(id)
-    fetched_service_data = fetch_user_services id
-    services = Service.where(id: fetched_service_data.keys).all
-    services.map do |service|
-      service.cost_month = fetched_service_data[service.id][:service_cost]
-      service
-    end
+    data = get_response get(route(:user_services, user_id: id))
+    ApiDataTranformer.transform_services_data data
   end
 
   private
 
-  def self.send_request(request)
+  def self.route(name, params = {})
+    route = ROUTES[name].clone
+    params.each { |k, v| route.gsub! k.to_s, v.to_s }
+    route
+  end
+
+  def self.get_response(request)
     response = request.parsed_response.with_indifferent_access
     raise Exceptions::RemoteRequestError.new "Remote server couldn't execute request" unless response[:success]
     response[:data]
+  end
+
+  module ApiDataTranformer
+    def self.transform_user_data(data)
+      data[:vist_account] = data[:account_id]
+    end
+
+    def self.transform_services_data(data)
+      data[:services].values.index_by { |service| service[:service_id] }
+    end
   end
 end
