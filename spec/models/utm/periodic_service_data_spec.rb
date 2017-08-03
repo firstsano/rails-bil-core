f8c98edee3ac5b7477f4e6e539d3a44247ab76fc
Koltunov Alexander <firstsano@gmail.com>
2017-08-03 17:33:53 +0300

    Remove deprecated factory, move cost_day to PeriodicServiceData

--- spec/models/utm/periodic_service_data_spec.rb
+++ spec/models/utm/periodic_service_data_spec.rb
@@ -8,5 +8,15 @@ RSpec.describe Utm::PeriodicServiceData, type: :model do
       it { is_expected.to respond_to(:cost_month) }
       its(:cost) { is_expected.to eq subject.cost_month }
     end
+
+    context "#cost_day" do
+      it { is_expected.to respond_to(:cost_day) }
+
+      it "should calculate cost_day based on cost_month" do
+        periodic_service_data = create :periodic_service_data, cost: 1000
+        expectable_cost = (1000.0 / Time.days_in_month(Date.current.month)).round 2
+        expect(periodic_service_data.cost_day).to eq expectable_cost
+      end
+    end
   end
 end
