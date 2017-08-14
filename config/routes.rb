209f3c6895a7977eb1c5a48ba87d7bc213051b1d
Koltunov Alexander <firstsano@gmail.com>
2017-08-14 13:16:02 +0300

    Change get to namespaces for grouping

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
   scope module: :v1 do
     post '/users/sign-in' => 'user_token#create'
-    get '/discounts/index'
-    get '/payments/index'
-    get '/users/show'
-    get '/services/index'
+
+    namespace :discounts do
+      get :index
+    end
+
+    namespace :payments do
+      get :index
+    end
+
+    namespace :users do
+      get :show
+    end
+
+    namespace :services do
+      get :index
+    end
   end
 end
