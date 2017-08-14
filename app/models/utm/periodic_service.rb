f61eb0bbaae1f0dc0fbebef508a466dbfd5eaaec
Koltunov Alexander <firstsano@gmail.com>
2017-08-14 15:37:27 +0300

    Add bilateral relationship

--- app/models/utm/periodic_service.rb
+++ app/models/utm/periodic_service.rb
@@ -1,4 +1,6 @@
 class Utm::PeriodicService < Utm::VolgaspotRecord
+  has_and_belongs_to_many :tariffs, class_name: "Utm::PeriodicService", join_table: "tariffs_services_link",
+    foreign_key: :tariff_id, association_foreign_key: :service_id
   self.table_name = "periodic_services_data"
 
   alias_attribute :cost_month, :cost
--- app/models/utm/tariff.rb
+++ app/models/utm/tariff.rb
@@ -1,6 +1,6 @@
 class Utm::Tariff < Utm::VolgaspotRecord
-  has_and_belongs_to_many :periodic_services, class_name: "Utm::PeriodicService", join_table: "tariffs_services_link",
-    foreign_key: :tariff_id, association_foreign_key: :service_id
+  has_and_belongs_to_many :tariffs, class_name: "Utm::Tariff", join_table: "tariffs_services_link",
+    foreign_key: :service_id, association_foreign_key: :tariff_id
 
   self.table_name = "tariffs"
 end
