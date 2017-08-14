dd3e2dd4137658f207f13e94761d673cb27be8fa
Koltunov Alexander <firstsano@gmail.com>
2017-08-14 15:34:19 +0300

    Refactor spec

--- spec/models/utm/tariff_spec.rb
+++ spec/models/utm/tariff_spec.rb
@@ -3,5 +3,9 @@ require "rails_helper"
 RSpec.describe Utm::Tariff, type: :model do
   it_behaves_like "readonly model"
 
-  it { is_expected.to respond_to(:periodic_services) }
+  describe "instance methods" do
+    context "#periodic_services" do
+      it { is_expected.to respond_to(:periodic_services) }
+    end
+  end
 end
