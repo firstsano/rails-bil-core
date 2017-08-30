f684c0ca43f77aa1be366ba4e7662b9339a7b003
Koltunov Alexander <firstsano@gmail.com>
2017-08-30 17:12:16 +0300

    Add base db to sequel mapper

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,29 +1,30 @@
 module RCore
   class SequelDb
     class << self
-      attr_reader :utm_db
+      attr_reader :base_db, :utm_db
 
       def connections
-        [@utm_db]
+        [@base_db, @utm_db]
       end
 
       def start_connections
+        @base_db ||= establish_connection
         @utm_db ||= establish_connection('utm')
       end
 
       def disconnect_all
-        utm_db.disconnect
+        connections.each(&:disconnect)
       end
 
       private
 
-      def establish_connection(database)
+      def establish_connection(database = nil)
         Sequel.connect params_to_connect(database)
       end
 
-      def params_to_connect(database)
+      def params_to_connect(database = nil)
         yml_config = YAML.load_file(File.expand_path('../../database.yml', __FILE__))
-        env_database = "#{database}_#{Rails.env}"
+        env_database = database ? "#{database}_#{Rails.env}" : Rails.env
         yml_config[env_database]
       end
     end
