4684f0af30b8a2f4fa7750af820eaa2b021f25a9
Koltunov Alexander <firstsano@gmail.com>
2017-09-01 15:17:25 +0300

    Fix Payment model

--- app/models/utm/payment.rb
+++ app/models/utm/payment.rb
@@ -1,10 +1,16 @@
-class Utm::Payment < Sequel::Model(RCore::SequelDb.utm_db[:payment_transactions])
+setup_table = RCore::SequelDb.utm_db[:payment_transactions]
+  .select(:id, :account_id, :payment_incurrency, :currency_id, :currency_rate, :payment_absolute,
+    :actual_date, :payment_enter_date, :payment_ext_number, :who_receive, :comments_for_user,
+    :comments_for_admins, :burn_time, :is_canceled, :method)
+
+class Utm::Payment < Sequel::Model(setup_table)
   include WithinRange
   include ReadOnlyRecords
 
+  def_column_alias :payment_method_id, :method
   within_range_attribute :payment_enter_date
 
-  many_to_one :payment_method, key: :method, primary_key: :id, class: Utm::PaymentMethod
+  many_to_one :payment_method, key: :payment_method_id, class: Utm::PaymentMethod
 
   # scope :by_user_account, -> account { where(account_id: account.id) }
 end
