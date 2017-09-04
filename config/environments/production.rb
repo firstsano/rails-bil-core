3bfc1c179d18f5f625460c604a575f65533c46aa
Koltunov Alexander <firstsano@gmail.com>
2017-09-04 19:07:28 +0300

    Setup ExceptionNotification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Configure ActionMailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        "mail.vgg.ru",
+    port:           587,
+    authentication: "login",
+    user_name:      "alarm@vgg.ru",
+    password:       "erh345iuyb",
+    enable_starttls_auto: true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
