1fa156c73c1ad5380bd0d91761c2d736407eb49b
Koltunov Alexander <firstsano@gmail.com>
2017-09-04 18:16:48 +0300

    Remove yet unnecessary initializer and self-links

--- config/initializers/jsonapi_mimetypes.rb
+++
@@ -1,4 +0,0 @@
-ActionDispatch::Http::Parameters::DEFAULT_PARSERS[:api_json] = lambda do |body|
-  JSON.parse(body)
-end
-ActionDispatch::Request.parameter_parsers = ActionDispatch::Request::DEFAULT_PARSERS
