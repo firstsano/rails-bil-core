e1cb561766b76b0a8e52c15a38f4b66af603b88a
Koltunov Alexander <firstsano@gmail.com>
2017-09-27 16:46:13 +0300

    Rubocopotify controllers

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -1,29 +1,32 @@
-class V1::BaseController < ActionController::API
-  include Knock::Authenticable
-  include ::ExceptionsHandler
+module V1
+  class BaseController < ::ActionController::API
+    include Knock::Authenticable
+    include ::ExceptionsHandler
 
-  before_action :authenticate_user
+    before_action :authenticate_user
 
-  def render(resource)
-    super json: serialize_model(resource)
-  end
+    def render(resource)
+      super json: serialize_model(resource)
+    end
 
-  def prepare_date_range
-    from, to = get_filter_params(:from, :to)
-    from, to = (from&.to_datetime or DateTime.now), to&.to_datetime
-    @date_range = DateTimeRange.generate start: from, stop: to
-  end
+    def prepare_date_range
+      from, to = get_filter_params(:from, :to)
+      from = from&.to_datetime || DateTime.now
+      to = to&.to_datetime
+      @date_range = DateTimeRange.generate start: from, stop: to
+    end
 
-  def serialize_model(model, options = {})
-    options[:is_collection] = model.is_a?(Array)
-    options[:include] = params[:include]
-    JSONAPI::Serializer.serialize(model, options)
-  end
+    def serialize_model(model, options = {})
+      options[:is_collection] = model.is_a?(Array)
+      options[:include] = params[:include]
+      JSONAPI::Serializer.serialize(model, options)
+    end
 
-  private
+    private
 
-  def get_filter_params(*attributes)
-    return attributes.count.times.map { nil } unless params[:filter]
-    params[:filter].values_at *attributes
+    def get_filter_params(*attributes)
+      return Array.new(attributes.count) { nil } unless params[:filter]
+      params[:filter].values_at(*attributes)
+    end
   end
 end
--- app/controllers/v1/payments_controller.rb
+++ app/controllers/v1/payments_controller.rb
@@ -1,11 +1,13 @@
-class V1::PaymentsController < V1::BaseController
-  before_action :prepare_date_range
+module V1
+  class PaymentsController < BaseController
+    before_action :prepare_date_range
 
-  def index
-    @payments = Utm::Payment
-      .within_range(@date_range)
-      .by_user_account(current_user.utm_account)
-      .all
-    render @payments
+    def index
+      @payments = Utm::Payment
+                  .within_range(@date_range)
+                  .by_user_account(current_user.utm_account)
+                  .all
+      render @payments
+    end
   end
 end
