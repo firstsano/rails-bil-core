ee087dc40e064463a6a66b41d598165bc0add6c2
Koltunov Alexander <firstsano@gmail.com>
2017-09-29 15:37:53 +0300

    Rubocopify service

--- app/lib/date_time_range.rb
+++ app/lib/date_time_range.rb
@@ -4,16 +4,17 @@ class DateTimeRange
   attr_reader :range, :start, :stop
 
   def initialize(start:, stop:)
-    @start, @stop = [start, stop]
+    @start = start
+    @stop = stop
     @range = @start..@stop
   end
 
   def self.generate_month(date)
-    self.new start: date.beginning_of_month, stop: date.end_of_month
+    new start: date.beginning_of_month, stop: date.end_of_month
   end
 
-  def self.generate(start: , stop: nil)
+  def self.generate(start:, stop: nil)
     return generate_month(start) unless stop
-    self.new start: start, stop: stop
+    new start: start, stop: stop
   end
 end
