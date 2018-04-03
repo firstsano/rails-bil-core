319e68f1b013995b92e02d344828df7c52b2d00a
Koltunov Alexander <firstsano@gmail.com>
2018-04-03 12:47:42 +0300

    Move routing test to namespace, add tariffs routing specs

---
+++ spec/routing/v1/tariffs_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "routes user tariffs information" do
+  subject(:tariff) { create :tariff }
+
+  describe "user current tariff detalization" do
+    it "should be routable" do
+      expect(get: "/tariffs/index").to be_routable
+    end
+  end
+
+  describe "user connect tariff" do
+    it "should be routable" do
+      expect(post: "/tariffs/index").to be_routable
+    end
+  end
+
+  describe "user disconnect tariff" do
+    it "should be routable" do
+      expect(delete: "tariffs/#{tariff.id}").to be_routable
+    end
+  end
+
+  describe "user change tariff" do
+    it "should be routable" do
+      expect(put: "tariffs/#{tariff.id}").to be_routable
+    end
+  end
+
+  describe "user available tariffs to change" do
+    it "should be routable" do
+      expect(get: "/tariffs/available").to be_routable
+    end
+  end
+end
