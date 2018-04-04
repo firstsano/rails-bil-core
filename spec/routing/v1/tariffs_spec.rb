be23f3d9d4ce47ce50f73a4f05fe8ac26c4ffca7
Koltunov Alexander <firstsano@gmail.com>
2018-04-04 17:28:12 +0300

    Change routing for tariffs

--- spec/routing/v1/tariffs_spec.rb
+++ spec/routing/v1/tariffs_spec.rb
@@ -5,25 +5,19 @@ RSpec.describe "routes user tariffs information" do
 
   describe "user current tariff detalization" do
     it "should be routable" do
-      expect(get: "/tariffs").to be_routable
-    end
-  end
-
-  describe "user connect tariff" do
-    it "should be routable" do
-      expect(post: "/tariffs").to be_routable
+      expect(get: "/tariffs/index").to be_routable
     end
   end
 
   describe "user disconnect tariff" do
     it "should be routable" do
-      expect(delete: "tariffs/#{tariff.id}").to be_routable
+      expect(delete: "/tariffs/index").to be_routable
     end
   end
 
   describe "user change tariff" do
     it "should be routable" do
-      expect(put: "tariffs/#{tariff.id}").to be_routable
+      expect(put: "/tariffs/index").to be_routable
     end
   end
 
